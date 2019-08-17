d188504fe136b5eb34e0b674842f5d0bd95b5f2c
Lena Maglo <maglo.lena@gmail.com>
2019-08-17 16:59:12 +0200

    trying to fix new

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -26,6 +26,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :photo, :instructions)
+    params.require(:cocktail).permit(:name, :photo, :instruction)
   end
 end
--- db/migrate/20190817092812_add_instructions_to_cocktails.rb
+++
@@ -1,5 +0,0 @@
-class AddInstructionsToCocktails < ActiveRecord::Migration[5.2]
-  def change
-    add_column :cocktails, :instructions, :text
-  end
-end
